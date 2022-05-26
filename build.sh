3f42e7351811e667c0fa4dc013320ff30fe9b4cc
miquelpascual <61166397+miquelpascual@users.noreply.github.com>
2022-05-26 18:59:10 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,18 +4,18 @@ HADOOP_VERSION="3.2"
 JUPYTERLAB_VERSION="3.2.0"
 
 # Creación de la imagen de Docker spark-master
-#docker build \
-#  --build-arg spark_version="${SPARK_VERSION}" \
-#  --build-arg hadoop_version="${HADOOP_VERSION}" \
-#  -f spark-master.Dockerfile \
-#  -t spark-master .
+docker build \
+  --build-arg spark_version="${SPARK_VERSION}" \
+  --build-arg hadoop_version="${HADOOP_VERSION}" \
+  -f spark-master.Dockerfile \
+  -t spark-master .
 
 # Creación de la imagen de Docker spark-worker
-#docker build \
-#  --build-arg spark_version="${SPARK_VERSION}" \
-#  --build-arg hadoop_version="${HADOOP_VERSION}" \
-#  -f spark-worker.Dockerfile \
-#  -t spark-worker .
+docker build \
+  --build-arg spark_version="${SPARK_VERSION}" \
+  --build-arg hadoop_version="${HADOOP_VERSION}" \
+  -f spark-worker.Dockerfile \
+  -t spark-worker .
 
 # Creación de la imagen de Docker jupyterlab
 docker build \
